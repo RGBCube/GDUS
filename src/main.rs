b5085996866630a75ef5c877264225cd77bd6881
RGBCube <RGBCube@users.noreply.github.com>
2023-11-10 14:06:33 +0300

    Add hello world Axum and Maud code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing, Router};
+use maud::{html, Markup};
+
+async fn index() -> Markup {
+    html! {
+        h1 { "Hello, World!" }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(index));
+
+    axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
